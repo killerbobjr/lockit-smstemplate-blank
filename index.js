30332f0398e3e3b7da003f60426c0a90b09e17c0
killerbobjr <github@stinkycats.net>
2016-02-17 05:38:17 -0800

    Simplified callback to only process content object

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
+var	ejs = require('ejs');
 
-var fs = require('fs');
-var path = require('path');
-var ejs = require('ejs');
-
-module.exports = function(title, content, cb) {
-  
-  // read html file
-  fs.readFile(path.join(__dirname, 'index.html'), 'utf8', function(err, data) {
-    if (err) return cb(err);
-    
-    // set local variables
-    var locals = {
-      title: title,
-      content: content
-    };
-    
-    // create html
-    var html = ejs.render(data, locals);
-    
-    // return modified html content
-    cb(null, html);
-  });
-  
+module.exports = function(options, next)
+{
+	// Format the sms content
+	next(undefined, undefined, ejs.render('<%- content %>', options));
 };
